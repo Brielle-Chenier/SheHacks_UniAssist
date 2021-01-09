57f1db097a3979e7bbdf7d231a79fe366ccb42e1
Brielle-Chenier <brielle_chenier@live.com>
2021-01-09 18:58:15 -0500

    Update searchButton.js

--- uniassist_shehacks/src/searchButton.js
+++ uniassist_shehacks/src/searchButton.js
@@ -1,37 +1,8 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
+//import TextField from '@material-ui/core/TextField';
 
 
 export class SearchBar extends React.Component {
-<<<<<<< HEAD
-  render(){
-    return(
-      <div>
-        <button type = "button" onClick = {this.props.onClick} class = "colorButton">
-        </button>
-
-        <TextField id="outlined-basic" label="Enter OUAC Code" variant="outlined" />
-      </div>
-    );
-  };
-}
-=======
-  componentDidMount() {
-
-    fetch("https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo")
-      .then(response => response.json())
-      .then(result => {
-          this.setState({
-            userName: result.name,
-            userAge: result.age
-          });
-
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-      )
-  }
 
     constructor(props) {
       super(props);
@@ -47,33 +18,74 @@ export class SearchBar extends React.Component {
         LowAvg: "",
         CompAvg: "",
         SuppApp: "",
-        Interview: ""
+        Interview: "",
+        value: ''
 
         };
 
       this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      //this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
       this.setState({value: event.target.value});
     }
 
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+   componentDidMount() {
+     console.log("mounted!")
+      fetch("https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo")
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+              code: result.Code,
+              userAge: result.age
+            });
+
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+        )
+    }
+
+    handleSubmit = () => {
+     alert('A name was submitted: ' + this.state.value);
+     this.setState({
+       code: '8'
+     });
+
+    /*  fetch("https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo")
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+              code: result.Code,
+              age: result.age
+            });
+
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+        )
+      */
+      //event.preventDefault();
     }
 
     render() {
+      console.log("render() method");
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
 
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Search" />
-        </form>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+              <input type="submit" value="Search" />
+          </form>
+
+              <div> Hello {this.state.code} </div>
+              <div> {this.state.age} </div>
+          </div>
       );
     }
-  }
->>>>>>> 7d1360ba25df4415e0aeb6dda0593346fa104868
+  };
