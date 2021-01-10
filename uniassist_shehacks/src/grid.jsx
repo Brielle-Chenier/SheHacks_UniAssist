c9407e539b7a2717bb4abe6990e5e33e3ad95648
Brielle-Chenier <brielle_chenier@live.com>
2021-01-10 02:30:58 -0500

    added links to specific programs

---
+++ uniassist_shehacks/src/grid.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchBar from './searchButtonOne';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+//import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function NestedGrid() {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <SearchBar link = "https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo-1"/>
+            <div>
+            <IconButton aria-label="delete">
+              <FavoriteIcon> </FavoriteIcon>
+            </IconButton>
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <SearchBar  link = "https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo-2"/>
+            <div>
+            <IconButton aria-label="delete">
+              <FavoriteIcon> </FavoriteIcon>
+            </IconButton>
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+           <SearchBar  link = "https://northamerica-northeast1-shehacks21.cloudfunctions.net/getSchoolInfo-3"/>
+           <div>
+           <IconButton >
+             <FavoriteIcon> </FavoriteIcon>
+           </IconButton>
+           </div>
+          </Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
