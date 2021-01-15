4982ffe45f950ad0876a4a547cd92acd5029fd59
IsabellaSchneider7 <71522975+IsabellaSchneider7@users.noreply.github.com>
2021-01-15 13:09:21 -0500

    Update finalCode.py

---
+++ uniassist_shehacks/Python Code/finalCode.py
@@ -0,0 +1,27 @@
+#test
+import json
+import pandas as pd
+
+def main():
+    request = "MEE"
+    z = hello_world(request)
+
+    #debugging
+    print(z)
+    
+def hello_world(request):
+    dataSet = pd.read_csv("DataSheet.tsv",sep="\t")
+
+    #replace empty spaces
+    dataSet.columns = [column.replace(" ", "_") for column in dataSet.columns]
+
+    #given program code
+    programCode = request
+
+    #search for code
+    dataSet.query(f'Code == "{programCode}"', inplace = True )
+
+    return(dataSet.to_json(orient = "records"))
+
+if __name__ == "__main__":
+    main()
